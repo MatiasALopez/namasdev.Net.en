fd5455fcd38a2d8c2ce0fafa04bc6b10403a7142
Matias Lopez <matutelopez@hotmail.com>
2026-04-09 17:01:54 -0300

    [main] Upgrade to net48+net9 project

--- namasdev.Net.en/nuget-gen-pub.cmd
+++ namasdev.Net.en/nuget-gen-pub.cmd
@@ -1,2 +1,7 @@
-nuget pack namasdev.Net.en.csproj -Properties Configuration=Release
-nuget add namasdev.Net.en.1.0.0.nupkg -source \\MATUASUS\nuget
+@echo off
+dotnet build --configuration Release
+dotnet pack --configuration Release --no-build
+for %%f in (bin\Release\*.nupkg) do (
+    powershell -NoProfile -Command "& { $pkg = '%%~nf'; if ($pkg -match '^(.+?)\.(\d+\.\d+\.\d+.*)$') { $id = $matches[1].ToLower(); $ver = $matches[2]; $feed = '\\MATUASUS\nuget\' + $id + '\' + $ver; $cache = $env:USERPROFILE + '\.nuget\packages\' + $id + '\' + $ver; if (Test-Path $feed) { Remove-Item -Recurse -Force $feed; Write-Host ('Removed feed: ' + $feed) }; if (Test-Path $cache) { Remove-Item -Recurse -Force $cache; Write-Host ('Removed cache: ' + $cache) } } }"
+    nuget add "%%f" -source \\MATUASUS\nuget
+)
